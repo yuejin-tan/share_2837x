85bf450840ba36548ac60ad184b0a3205b87b103
yuejinTan <498339337@qq.com>
2023-12-04 22:53:03 +0800

    初步写完OECA

--- algo/oeca.h
+++ algo/oeca.h
@@ -14,14 +14,19 @@
 #include "filter.h"
 
  /// 用0-1代表0-2pi角度
+
 enum OECA_Sta
 {
-    OECA_stop = 0,
+    OECA_Ready = 0,
     OECA_Isweep0,
     OECA_Isweep1,
     OECA_algo0,
-    OECA_algo1,
+    OECA_stop,
 };
+
+#define OECA_M_AUTOALGO 1
+#define OECA_M_AUTO_PI_PARA 2
+#define OECA_M_AUTO_INIT_VAL 4
 
 struct OECA_struct
 {
@@ -31,6 +36,11 @@ struct OECA_struct
     float  intg_eComp;
 
     float theta_eComp;
+
+    float T_sigma_eComp;
+    float h_eComp;
+
+    float PICurrent;
 
     // PLL
     float  kp_pll;
@@ -48,16 +58,21 @@ struct OECA_struct
     // I-THETA SWEEP
     float sweepCurrent;
 
-    float* dbgBuffPos;
+    float estAns1;
+    float estAns2;
 
-    int32_t sweepCnt;
-    int32_t sweepMax;
+    float* hDbgBuff;
+
+    int sweepCnt;
+    int sweepMax;
 
     int sampDiv;
     int sampCnt;
 
     // control sta
     int status;
+
+    int modeCfg;
 
     struct LPF_Ord1_2_struct spdFilt1;
     struct LPF_Ord1_2_struct spdFilt2;
@@ -88,29 +103,38 @@ static inline void OECA_init(struct OECA_struct* hOECA)
 
     hOECA->theta_eComp = 0;
 
+    hOECA->T_sigma_eComp = 1.0f / 2.0f / MATLAB_PARA_pi1 / (100.0f * 0.5f) + 1.0f / 6000.0f * 5.0f;
+    hOECA->h_eComp = 10.0f;
+    hOECA->PICurrent = 30.0f;
+
     hOECA->kp_pll = 444.3;
     hOECA->ki_pll = 3.29;
     hOECA->intg_pll = 0;
 
     hOECA->theta_pll = 0;
 
-    hOECA->sampCnt = 0;
-    hOECA->sampDiv = 40 * 5;
-
-    hOECA->sweepCurrent = 30;
-    hOECA->sweepCnt = 0;
-    hOECA->sweepMax = 100 / 5;
-
-    hOECA->dbgBuffPos = (void*)0xC000ul;
-
     hOECA->lastThetaIn = 0;
     hOECA->omegaOB = 0;
+
     hOECA->lastOmegaOB = 0;
     hOECA->alphaOB = 0;
 
+    hOECA->sweepCurrent = 30;
+
+    hOECA->estAns1 = 0;
+    hOECA->estAns1 = 0;
+
+    hOECA->hDbgBuff = (float*)(void*)0xC000ul;
+
+    hOECA->sweepCnt = 0;
+    hOECA->sweepMax = 100 / 5;
+
+    hOECA->sampCnt = 0;
+    hOECA->sampDiv = 30 * 5;
 
     // control sta
     hOECA->status = OECA_stop;
+    hOECA->modeCfg = 0;
 
 #define LPF_2Ord_200Hz_T30000Hz_ksi0_70_PARA 0.0017038908F, -1.9413394F, 0.94304332F
 
@@ -131,33 +155,49 @@ static inline float OECA_util_angle_norm2(float angle)
     return angle - ceilf(angle + 0.5f);
 }
 
-static inline float OECA_pllCalc(struct OECA_struct* hOECA, float theta)
+static inline float OECA_pllCalc(struct OECA_struct* hOECA)
 {
-    float deltaTheta = OECA_util_angle_norm2(theta - hOECA->theta_pll);
+    float deltaTheta = OECA_util_angle_norm2(hOECA->theta_eComp - hOECA->theta_pll);
     hOECA->intg_pll = hOECA->intg_pll + deltaTheta * hOECA->ki_pll;
     float outPll = deltaTheta * hOECA->kp_pll + hOECA->intg_pll;
     hOECA->theta_pll = OECA_util_angle_norm(hOECA->theta_pll + outPll * (float)(1.0 / CTRL_FREQ));
     return hOECA->theta_pll;
 }
 
-static inline float OECA_PICalc(struct OECA_struct* hOECA, float omegaIn)
+static inline float OECA_PICalc(struct OECA_struct* hOECA)
 {
-    hOECA->intg_eComp = OECA_util_angle_norm(hOECA->intg_eComp + omegaIn * hOECA->ki_eComp);
-    hOECA->theta_eComp = OECA_util_angle_norm(hOECA->intg_eComp + omegaIn * hOECA->kp_eComp);
+    hOECA->intg_eComp = OECA_util_angle_norm(hOECA->intg_eComp + hOECA->omegaOB * hOECA->ki_eComp);
+    hOECA->theta_eComp = OECA_util_angle_norm(hOECA->intg_eComp + hOECA->omegaOB * hOECA->kp_eComp);
     return OECA_util_angle_norm(hOECA->theta_eComp + 0.5f);
+}
+
+static inline void OECA_algoStaSet(struct OECA_struct* hOECA, float initVal)
+{
+    hOECA->intg_eComp = initVal;
+    hOECA->theta_eComp = initVal;
+    hOECA->intg_pll = 0.0f;
+    hOECA->theta_pll = initVal;
+}
+
+static inline float OECA_getSweepTheta(struct OECA_struct* hOECA)
+{
+    return __divf32((float)hOECA->sweepCnt, (float)hOECA->sweepMax);
 }
 
 static inline float OECA_omegaOB(struct OECA_struct* hOECA, float thetaIn)
 {
-    float omegaDiffed = OECA_util_angle_norm2(thetaIn - hOECA->lastThetaIn) * (float)(CTRL_FREQ * MATLAB_PARA_pi2 / 65536.0);
+    float omegaRaw = OECA_util_angle_norm2(thetaIn - hOECA->lastThetaIn) * (float)(CTRL_FREQ * MATLAB_PARA_pi2);
     hOECA->lastThetaIn = thetaIn;
-    hOECA->omegaOB = LPF_Ord2_update_kahan(hOECA->spdFilt1, omegaDiffed);
+    hOECA->omegaOB = LPF_Ord2_update(&hOECA->spdFilt1, omegaRaw);
     return hOECA->omegaOB;
 }
 
 static inline float OECA_alphaOB(struct OECA_struct* hOECA)
 {
-
+    float alphaRaw = (hOECA->omegaOB - hOECA->lastOmegaOB) * (float)(CTRL_FREQ);
+    hOECA->lastOmegaOB = hOECA->omegaOB;
+    hOECA->alphaOB = LPF_Ord2_update(&hOECA->spdFilt2, alphaRaw);
+    return hOECA->alphaOB;
 }
 
 static inline void Goertz_init(struct Goertz_struct* hGeotz, int k, int N)
@@ -165,10 +205,10 @@ static inline void Goertz_init(struct Goertz_struct* hGeotz, int k, int N)
     hGeotz->k = k;
     hGeotz->N = N;
 
-    float N_1 = __divf32(1.0f, (float)hGeotz->N);
+    float k_N = __divf32((float)k, (float)N);
 
-    hGeotz->sinVal = sinf((float)(MATLAB_PARA_pi2) * (float)k * (float)N_1);
-    hGeotz->cosVal = cosf((float)(MATLAB_PARA_pi2) * (float)k * (float)N_1);
+    hGeotz->sinVal = __sinpuf32(k_N);
+    hGeotz->cosVal = __cospuf32(k_N);
 
     hGeotz->wTab[0] = 0;
     hGeotz->wTab[1] = 0;
@@ -179,6 +219,13 @@ static inline void Goertz_init(struct Goertz_struct* hGeotz, int k, int N)
     hGeotz->AnsAbs = 0;
     hGeotz->AnsReal = 0;
 
+}
+
+static inline void Goertz_reset(struct Goertz_struct* hGeotz)
+{
+    hGeotz->wTab[0] = 0;
+    hGeotz->wTab[1] = 0;
+    hGeotz->wTab[2] = 0;
 }
 
 static inline float Goertz_iter(struct Goertz_struct* hGeotz, float inVal)
