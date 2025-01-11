dcca84591e2911b5e5076a5e0219960b12b4db01
yuejinTan <498339337@qq.com>
2025-01-11 22:03:57 +0800

    改进离线辨识算法

--- algo/lms_anf.h
+++ algo/lms_anf.h
@@ -54,30 +54,7 @@ static inline float LMSanfUpdate(struct LMSanf_struct* hLMSanf, uint16_t angle, 
     return y;
 }
 
-static inline uint16_t thetaHarComp(struct LMSanf_struct* hLMSanf, uint16_t angle)
-{
-    float thetaPU1 = ((uint16_t)(angle * 1)) * (float)(1.0f / 65536.0f);
-    float thetaPU2 = ((uint16_t)(angle * 2)) * (float)(1.0f / 65536.0f);
-    float thetaPU4 = ((uint16_t)(angle * 4)) * (float)(1.0f / 65536.0f);
-    // [cos1 sin1 cos2 sin2 cos4 sin4]
-    float X[] = { __cospuf32(thetaPU1),__sinpuf32(thetaPU1),__cospuf32(thetaPU2),__sinpuf32(thetaPU2),__cospuf32(thetaPU4),__sinpuf32(thetaPU4), };
-
-    float y = 0;
-    // 若在CCS环境中，提示编译器展开循环
-#ifndef TYJ_TEST
-#pragma UNROLL(6)
-#endif
-    for (int i = 0;i < 6;i++)
-    {
-        y += X[i] * hLMSanf->W[i];
-    }
-
-    y *= (float)(65536.0 / M_PI / 2.0);
-
-    return y;
-}
-
-static inline float harmInjUtil(uint16_t anglPuQ16, uint16_t order, float Acos, float Asin)
+static inline float harmCalcUtil(uint16_t anglPuQ16, uint16_t order, float Acos, float Asin)
 {
     float ang = ((uint16_t)(anglPuQ16 * order)) * (float)(1.0f / 65536.0f);
     return Acos * __cospuf32(ang) + Asin * __sinpuf32(ang);
